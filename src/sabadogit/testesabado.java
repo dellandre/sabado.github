b920d86d187f10eddd86ce13b22b8ca334fc5b4f
Andre Dias <andredias73@gmail.com>
2018-09-29 20:15:07 -0300

    Mensagem criada boa tarde

--- src/sabadogit/testesabado.java
+++ src/sabadogit/testesabado.java
@@ -5,6 +5,9 @@ public class testesabado {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Bom dia");
+		System.out.println("Boa tarde");
+		
+		
 
 	}
 
